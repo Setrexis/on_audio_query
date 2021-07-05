5c73bd57e78e2da6acbf67c1e3228cf9596f2fda
Sebastian Nadler <33389982+Setrexis@users.noreply.github.com>
2021-07-05 20:38:32 +0200

    Update playlist_model.dart

--- lib/details/models/playlist_model.dart
+++ lib/details/models/playlist_model.dart
@@ -24,5 +24,5 @@ class PlaylistModel {
   /// Some times [dateModified] is returned null
   String? get dateModified => _info["date_modified"];
 
-  List<String> get memberIDs => _info["memberIDs"];
+  List<Object?> get memberIDs => _info["memberIDs"];
 }
